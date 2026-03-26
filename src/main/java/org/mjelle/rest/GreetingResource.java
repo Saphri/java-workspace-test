9cc3a3e0c2a75bb0ae0dc27065371491edcfde79
Martin Nyholm Jelle <mjelle@hotmail.com>
2026-03-26 18:28:01 +0100

    Refactor messaging components: remove unused classes and update configuration for NATS integration

--- src/main/java/org/mjelle/rest/GreetingResource.java
+++ src/main/java/org/mjelle/rest/GreetingResource.java
@@ -1,18 +1,12 @@
 package org.mjelle.rest;
 
-import java.time.Duration;
 import java.util.UUID;
-import java.util.concurrent.atomic.AtomicReference;
 
 import org.eclipse.microprofile.reactive.messaging.Channel;
 import org.eclipse.microprofile.reactive.messaging.Emitter;
 import org.eclipse.microprofile.reactive.messaging.Message;
-import org.mjelle.service.TaskConsumerConfiguration;
 
-import io.quarkiverse.reactive.messaging.nats.jetstream.client.Connection;
-import io.quarkiverse.reactive.messaging.nats.jetstream.client.ConnectionFactory;
 import io.quarkiverse.reactive.messaging.nats.jetstream.client.api.PublishMessageMetadata;
-import io.quarkiverse.reactive.messaging.nats.jetstream.configuration.JetStreamConfiguration;
 import io.smallrye.common.annotation.RunOnVirtualThread;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
@@ -22,15 +16,9 @@ import jakarta.ws.rs.core.MediaType;
 @Path("/hello")
 @RunOnVirtualThread
 public class GreetingResource {
-    private final ConnectionFactory connectionFactory;
-    private final JetStreamConfiguration natsConfiguration;
     private final Emitter<String> emitter;
 
-    private final AtomicReference<Connection> messageConnection = new AtomicReference<>();
-
-    public GreetingResource(ConnectionFactory connectionFactory, JetStreamConfiguration natsConfiguration, @Channel("task-queue-out") Emitter<String> emitter) {
-        this.connectionFactory = connectionFactory;
-        this.natsConfiguration = natsConfiguration;
+    public GreetingResource(@Channel("data-out") Emitter<String> emitter) {
         this.emitter = emitter;
     }
 
@@ -38,23 +26,7 @@ public class GreetingResource {
     @Produces(MediaType.TEXT_PLAIN)
     public void seedTasks() {
         final var uuid = UUID.randomUUID();
-        final var conn = getOrEstablishMessageConnection();
-        
-        final var conf = new TaskConsumerConfiguration("task." + uuid.toString(), Duration.ofSeconds(30));
-        conn.addConsumerIfAbsent("task-queue", "task-consumer-" + uuid.toString(), conf).await().indefinitely();
 
-        final var metadata = PublishMessageMetadata.builder()
-          .subject("task." + uuid.toString()).build();
-
-        emitter.send(Message.of("Task added!").addMetadata(metadata));
+        emitter.send(Message.of(uuid.toString()));
     }
-
-  private Connection getOrEstablishMessageConnection() {
-    var connection = messageConnection.get();
-    if (connection == null) {
-      connection = connectionFactory.create(natsConfiguration.connection()).await().indefinitely();
-      messageConnection.set(connection);
-    }
-    return connection;
-  }
 }
--- src/main/java/org/mjelle/scheduler/TaskScheduler.java
+++ src/main/java/org/mjelle/scheduler/TaskScheduler.java
@@ -1,16 +1,8 @@
 package org.mjelle.scheduler;
 
-import java.io.IOException;
-import java.util.concurrent.atomic.AtomicReference;
-
+import org.eclipse.microprofile.reactive.messaging.Incoming;
 import org.jboss.logging.Logger;
 
-import io.nats.client.JetStreamApiException;
-import io.nats.client.api.StreamInfoOptions;
-import io.quarkiverse.reactive.messaging.nats.jetstream.client.Connection;
-import io.quarkiverse.reactive.messaging.nats.jetstream.client.ConnectionFactory;
-import io.quarkiverse.reactive.messaging.nats.jetstream.configuration.JetStreamConfiguration;
-import io.quarkus.scheduler.Scheduled;
 import io.smallrye.common.annotation.RunOnVirtualThread;
 import jakarta.enterprise.context.ApplicationScoped;
 
@@ -19,52 +11,9 @@ public class TaskScheduler {
   
   private final Logger log = Logger.getLogger(TaskScheduler.class);
 
-  private final ConnectionFactory connectionFactory;
-  private final JetStreamConfiguration natsConfiguration;
-
-  private final AtomicReference<Connection> messageConnection = new AtomicReference<>();
-
-  public TaskScheduler(ConnectionFactory connectionFactory, JetStreamConfiguration natsConfiguration) {
-    this.connectionFactory = connectionFactory;
-    this.natsConfiguration = natsConfiguration;
-  }
-
-  @Scheduled(every = "10s", concurrentExecution = Scheduled.ConcurrentExecution.SKIP)
+  @Incoming("data-in")
   @RunOnVirtualThread
-  public void scheduleTask() {
-    log.info("Task scheduler running");
-
-    final var conn = getOrEstablishMessageConnection();
-
-    conn.nativeConnection(c -> {
-      try {
-        final var streamState = c.jetStreamManagement().getStreamInfo("task-queue", StreamInfoOptions.allSubjects()).getStreamState();
-
-        streamState.getSubjectMap().entrySet().stream().forEach(subjectState -> {
-          log.infof("SubjectState name: %s, messages: %d", subjectState.getKey(), subjectState.getValue());
-
-          final var msg = "Resource event for subject " + subjectState.getKey();
-          final var subject = subjectState.getKey().replace("task.", "resource-event.");
-
-          try {
-            c.jetStream().publish(subject, null, msg.getBytes());
-            log.warnf("Sent Resource event on subject: %s", subject);
-          } catch (IOException | JetStreamApiException e) {
-            log.debugf(e, "Exception publishing message to subject: %s", subject);
-          }
-        });
-      } catch (IOException | JetStreamApiException e) {
-        log.warnf(e, "Exception creating stream task-queue");
-      }
-    });
-  }
-
-  private Connection getOrEstablishMessageConnection() {
-    var connection = messageConnection.get();
-    if (connection == null) {
-      connection = connectionFactory.create(natsConfiguration.connection()).await().indefinitely();
-      messageConnection.set(connection);
-    }
-    return connection;
+  public void onResourceEvent(String msg) {
+    log.infof("onResourceEvent running: %s", msg);
   }
 }
